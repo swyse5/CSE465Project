ea82d0985c63190026b025af2affc5b92d61da4a
swyse5 <stuart.wyse@gmail.com>
2016-11-27 14:19:17 -0500

    fixed main function - parseStatement and file done (for now)

--- zpm/zpm.cpp
+++ zpm/zpm.cpp
@@ -5,30 +5,56 @@
 #include <string>
 #include <fstream>
 #include <iostream>
-
-using namespace std;
+#include <cstring>
+#inlcude "zpm.h"
 
 int count = 0;
 
-void parseStatement(string line) {
-  
+// Constructor
+zpm::zpm() {
+
+}
+
+// Destructor
+zpm::~zpm() {
+  deletePointers();
+}
+
+// Finds the correct funtion to send the line to intepret it
+void zpm::parseStatement(std::string line) {
+  // if PRINT is not on a line, we know it's an assignment statement
+  if(line.find("PRINT") == -1) {
+    analyzeAssignment(line);
+  } else {
+    analyzePrint(line);
+  }
+}
+
+void zpm::parseFile(char* file) {
+  std::ifstream zpmFile;
+  zpmFile.open(file);
+
+  // parse each statement in the file
+  std::string line = "";
+  while(getline(zpmFile, line)) {
+    lineNumber++;
+    parseStatement(line);
+  }
 }
 
 int main(int argc, char* argv[]) {
   if(argc <= 1) {
-    cout << "ERROR: File name missing.\n";
+    std::cerr << "ERROR: File name missing.\n" << std::endl;
     return -1;
   }
+  
+  char* zpmFileName = argv[1];
+  zpm* interpreter = new zpm();
 
-  ifstream infile(argv[1]); // open the file
+  interpreter->parseFile(zpmFileName);
 
-  if(infile.is_open() && infile.good()) {
-    string line = "";
-    while(getLine(infile, line)) {
-      count++;
-      parseStatement(line);
-    }
-  }
+  delete interpreter;
+  interpreter = NULL;
 
   return 0;
 }
