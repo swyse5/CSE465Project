6eddb64eab3c475cec953c7e32d8b2e8c997dcc2
swyse5 <stuart.wyse@gmail.com>
2016-11-27 16:55:28 -0500

    more functions added - now time to implement the bulk of the work

--- zpm/zpm.cpp
+++ zpm/zpm.cpp
@@ -42,12 +42,42 @@ void zpm::parseFile(char* file) {
   }
 }
 
+// Deletes the varTable pointers
+// This is used in the destructor and runtimeError function
+void zpm::deletePointers() {
+  typedef std::map<std::string, Data>::iterator it_type;
+  for(it_type iterator = varTable.begin(); iterator != varTable.end(); iterator++) {
+    delete iterator->second.s;
+    iterator->second.s = NULL;
+  }
+}
+
+// If there is leading or trailing whitespace on a string, this trims that out
+// Got some help from StackOverflow to do this
+std::string zpm::trimWhitespace(std::string* str) {
+  int start = str->find_first_not_of(" ");
+  int end = str->find_last_not_of(" ");
+  return str->substr(begin, (end-begin+1));
+}
+
+// Takes in a string and returns that string, but without the quotes ("") around it
+std::string zpm::trimQuotes(std::string str) {
+  return str.substr(1, str.length() - 2);
+}
+
+// Throw runtime error with line number of error
+void zpm::runtimeError() {
+  std:cerr <<"Runtime error on line: " <<lineNumber <<std::endl;
+  deletePointers();
+  exit(1);
+}
+
 int main(int argc, char* argv[]) {
   if(argc <= 1) {
     std::cerr << "ERROR: File name missing.\n" << std::endl;
     return -1;
   }
-  
+
   char* zpmFileName = argv[1];
   zpm* interpreter = new zpm();
 
